4adc167e41be76c409c4d59a4a6b3149eef2a1dd
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-01 17:57:19 -0300

    Entendendo o async com o await

---
+++ exemplo6.ts
@@ -0,0 +1,17 @@
+import fs from 'fs/promises'
+
+fs.readFile('./bancodedados.json').then((resultado) => {
+    const json = resultado.toString()
+    const bancodedados = JSON.parse(json)
+
+    bancodedados.usuarios.push({
+        nome: "João",
+        email: 'joao@email.com'
+    })
+
+    fs.writeFile('./bancodedados.json', JSON.stringify(bancodedados))
+
+
+}).catch(()=>{
+    console.log('Arquivo não existe!')
+})
---
+++ exemplo7.ts
@@ -0,0 +1,15 @@
+import fs from 'fs/promises'
+
+async function principal() {
+    // const bancodedados = await fs.readFile('./bancodedados.json')
+    // console.log(bancodedados)
+
+    await fs.writeFile('./teste.txt', 'Adriano Allef')
+
+    const arquivo = await fs.readFile('teste.txt')
+
+    console.log(arquivo.toString());
+    
+}
+
+principal()
