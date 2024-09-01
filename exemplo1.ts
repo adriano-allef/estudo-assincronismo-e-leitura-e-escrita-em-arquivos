48567a9434d1cb3132fb18d07c7931f0cf95a121
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-01 15:24:11 -0300

    Exercicio 1 - entendendo execução assinc e sing e o conceito de promessa

---
+++ exemplo1.ts
@@ -0,0 +1,25 @@
+// console.log('primeira etapa')
+
+// setTimeout(() => {
+//     console.log('segunda etapa')
+// }, 3000)
+
+// console.log('terceira etapa')
+
+// Promessas (Promises)
+// Pending -> pendente | Fulfield -> Resolvida | Rejected -> Rejeitada
+
+console.log('primeira etapa')
+
+async function imprimir(texto){
+    return new Promise((resolver, rejeitar) => {
+        resolver(texto)
+    })
+}
+
+imprimir('segunda etapa').then((resultado) => {
+    console.log(resultado)
+})
+
+console.log('terceira etapa');
+
