fef91dc231183ec514d5386bad2605d2c1fd2c94
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-01 16:51:56 -0300

    Exercicio 4

---
+++ exemplo4.ts
@@ -0,0 +1,26 @@
+import fs from 'fs/promises'
+
+console.log('Antes de ler o arquivo')
+
+const texto1 = fs.readFile('./texto1.txt')
+
+const texto2 = fs.readFile('./texto2.txt')
+
+texto1.then((resultado) =>{
+    console.log(resultado.toString())
+})
+
+texto2.then((resultado) =>{
+    console.log(resultado.toString())
+})
+
+fs.writeFile('./teste.txt', 'Guido Cerqueira').then(() => {
+    const teste = fs.readFile('./teste.txt')
+    teste.then((resultado) => {
+        console.log(resultado.toString())
+    })
+})
+
+
+
+console.log('Depois de ler o arquivo')
