38b9e431a28a3c3d57f3e9906efe94d5fb2c919c
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-07 15:11:50 -0300

    Assincronismo e Leitura e Escrita de Arquivos

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,24 @@ app.get('/', async (req, res) => {
 
 
 
-    return res.json(resultado)
+    return res.json(resultado.usuarios)
+})
+
+app.post('/usuarios', async (req, res) => {
+    const { nome, email } = req.body
+
+    const bancodedados = await fs.readFile('bancodedados.json')
+
+    const resultado = JSON.parse(bancodedados.toString())
+
+    resultado.usuarios.push({
+        nome, email
+    })
+
+    await fs.writeFile('bancodedados.json', JSON.stringify(resultado))
+
+    return res.status(201).json({ nome, email })
+
 })
 
 app.listen(3000)
